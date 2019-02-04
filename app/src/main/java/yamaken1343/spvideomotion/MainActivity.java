3cb49a576755ba0dddeb6a595f9cd4d5a55f88ec
yamaken <yamaken1343@gmail.com>
2019-02-04 17:55:20 +0900

    被験者の名前と動画のIDをVideoPlayに渡すように

--- app/src/main/java/yamaken1343/spvideomotion/MainActivity.java
+++ app/src/main/java/yamaken1343/spvideomotion/MainActivity.java
@@ -1,7 +1,5 @@
 package yamaken1343.spvideomotion;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +10,13 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     public static final String VIDEOFQDN = "yamaken1343.spvideomotion.VIDEOFQDN";
+    public static final String VIDEOID = "yamaken1343.spvideomotion.VIDEOID";
+    public static final String USERNAME = "yamaken1343.spvideomotion.USERNAME";
     Button startButton;
     String videoUrl;
     String videoId;
     String videoFqdn;
+    String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +35,13 @@ public class MainActivity extends AppCompatActivity {
 
                 videoFqdn = videoUrl+videoId;
 
+                EditText un = findViewById(R.id.user_name_field);
+                userName = un.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, VideoPlay.class);
                 intent.putExtra(VIDEOFQDN, videoFqdn);
+                intent.putExtra(USERNAME, userName);
+                intent.putExtra(VIDEOID, videoId);
                 startActivity(intent);
 
 
